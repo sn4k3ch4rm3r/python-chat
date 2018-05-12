dd52f44cb2932b00b9bfd7af54228c08f47d57f9
Boldizsár Tóth <bocko200@gmail.com>
2018-05-12 15:15:54 +0200

    Fixing errors

--- update.py
+++ update.py
@@ -4,19 +4,23 @@ from os import system
 
 def main():
 	try:
-		chat = urllib.urlopen('https://raw.githubusercontent.com/toth-boldizsar/python-chat/master/chat.py').read().decode('utf-8')
-		updater = urllib.urlopen('https://raw.githubusercontent.com/toth-boldizsar/python-chat/master/update.py').read().decode('utf-8')
-		version = urllib.urlopen('https://raw.githubusercontent.com/toth-boldizsar/python-chat/master/version.txt').read().decode('utf-8')
-	except urllib.URLError as e:
+		chat = urllib.request.urlopen('https://raw.githubusercontent.com/toth-boldizsar/python-chat/master/chat.py').read().decode('utf-8')
+		updater = urllib.requesr.urlopen('https://raw.githubusercontent.com/toth-boldizsar/python-chat/master/update.py').read().decode('utf-8')
+		version = urllib.request.urlopen('https://raw.githubusercontent.com/toth-boldizsar/python-chat/master/version.txt').read().decode('utf-8')
+	except urllib.request.URLError as e:
 		print('Connection failed')
 		exit()
 	oldversion = open('version.txt', 'r').read()
 
 	try:
 		system('mkdir versions')
-	except Exception as e:
+	except Exception:
 		pass
-	system('mkdir versions/'+oldversion)
+	try:
+		system('mkdir versions/'+oldversion)
+	except Exception:
+		pass
+
 	system('mv chat.py versions/'+oldversion)
 	system('mv version.txt versions/'+oldversion)
 
