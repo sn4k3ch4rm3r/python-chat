520ac2591045a45e42c9d214af5d99878bba4d9c
Boldizsár Tóth <bocko200@gmail.com>
2018-05-12 15:00:36 +0200

    Previous versions

--- update.py
+++ update.py
@@ -5,23 +5,30 @@ from os import system
 def main():
 	try:
 		chat = urllib.urlopen('https://raw.githubusercontent.com/toth-boldizsar/python-chat/master/chat.py').read().decode('utf-8')
+		updater = urllib.urlopen('https://raw.githubusercontent.com/toth-boldizsar/python-chat/master/update.py').read().decode('utf-8')
 		version = urllib.urlopen('https://raw.githubusercontent.com/toth-boldizsar/python-chat/master/version.txt').read().decode('utf-8')
 	except urllib.URLError as e:
 		print('Connection failed')
 		exit()
 	oldversion = open('version.txt', 'r').read()
 
-	system('mkdir versions')
+	try:
+		system('mkdir versions')
+	except Exception as e:
+		pass
 	system('mkdir versions/'+oldversion)
 	system('mv chat.py versions/'+oldversion)
 	system('mv version.txt versions/'+oldversion)
 
 	chatFile = open('chat.py', 'w+')
+	updaterFile = open('update.py', 'w')
 	versionFile = open('version.txt', 'w+')
 
 	chatFile.write(chat)
+	updaterFile.write(updater)
 	versionFile.write(version)
 	chatFile.close()
+	updaterFile.close()
 	versionFile.close()
 
 	system('chmod +x chat.py')
